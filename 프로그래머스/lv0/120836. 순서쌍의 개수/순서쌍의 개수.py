4969a202ac88927411fcd55cc6b21bcb6dfd6572
Eunchan Jeong <89077219+EunchanJeong@users.noreply.github.com>
2023-09-05 17:11:53 +0900

    [level 0] Title: 순서쌍의 개수, Time: 56.64 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.py
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.py
@@ -1,17 +1,23 @@
 def solution(n):
-    a = n//2
+#     a = n//2
                     
+#     answer = 0
+#     limit = a
+    
+#     for i in range(1, a+1):
+#         for j in range(1, limit+1):
+#             if i * j == n:
+#                 answer += 1
+#                 limit = j
+#                 break
+#     answer += 2
+    
+#     if n == 1:
+#         answer = 1
+    
     answer = 0
-    limit = a
-    
-    for i in range(1, a+1):
-        for j in range(1, limit+1):
-            if i * j == n:
-                answer += 1
-                limit = j
-                break
-    answer += 2
-    
-    if n == 1:
-        answer = 1
+    for i in range(1, n+1):
+        if n % i == 0:
+            answer += 1
+
     return answer
