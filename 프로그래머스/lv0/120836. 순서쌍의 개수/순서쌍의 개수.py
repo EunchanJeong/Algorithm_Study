e8583732282b78255d6e134c8cdf566c40d4b5d1
Eunchan Jeong <89077219+EunchanJeong@users.noreply.github.com>
2023-09-05 17:09:11 +0900

    [level 0] Title: 순서쌍의 개수, Time: 894.17 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.py
@@ -0,0 +1,17 @@
+def solution(n):
+    a = n//2
+                    
+    answer = 0
+    limit = a
+    
+    for i in range(1, a+1):
+        for j in range(1, limit+1):
+            if i * j == n:
+                answer += 1
+                limit = j
+                break
+    answer += 2
+    
+    if n == 1:
+        answer = 1
+    return answer
