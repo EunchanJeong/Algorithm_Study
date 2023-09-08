040a2da95060e677e488edecf8c20503b4a4e97c
Eunchan Jeong <89077219+EunchanJeong@users.noreply.github.com>
2023-09-08 09:47:00 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -1,13 +1,8 @@
 def solution(price):
-    answer = 0
-    
     if price >= 500000:
-        answer = price * 0.8
+        price = price * 0.8
     elif price >= 300000:
-        answer = price * 0.9
+        price = price * 0.9
     elif price >= 100000:
-        answer = price * 0.95
-    else:
-        answer =  price
-        
-    return int(answer)
+        price = price * 0.95
+    return int(price)
