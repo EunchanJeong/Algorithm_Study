e9f93421a51c42a1e1398560acc171d82fe8bda5
Eunchan Jeong <89077219+EunchanJeong@users.noreply.github.com>
2023-09-13 14:17:40 +0900

    [unrated] Title: 홀수 vs 짝수, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.py
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.py
@@ -1,5 +1,5 @@
 def solution(num_list):
-    sums = []
-    sums.append(sum([val for i, val in enumerate(num_list) if i%2!=0]))
-    sums.append(sum([val for i, val in enumerate(num_list) if i%2==0]))
-    return max(sums)
+    # sums = []
+    # sums.append(sum([val for i, val in enumerate(num_list) if i%2!=0]))
+    # sums.append(sum([val for i, val in enumerate(num_list) if i%2==0]))
+    return max(sum(num_list[::2]), sum(num_list[1::2]))
