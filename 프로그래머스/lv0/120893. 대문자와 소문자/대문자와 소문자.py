330bb7d315dc04b4fbfd3dc1c49244e44a689584
Eunchan Jeong <89077219+EunchanJeong@users.noreply.github.com>
2023-09-14 11:49:50 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.py
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -1,8 +1,11 @@
+# def solution(my_string):
+    # my_string = list(my_string)
+    # for i, val in enumerate(my_string):
+    #     if val.isupper():
+    #         my_string[i] = val.lower()
+    #     else:
+    #         my_string[i] = val.upper()
+    # return ''.join(my_string)
+    
 def solution(my_string):
-    my_string = list(my_string)
-    for i, val in enumerate(my_string):
-        if val.isupper():
-            my_string[i] = val.lower()
-        else:
-            my_string[i] = val.upper()
-    return ''.join(my_string)
+    return my_string.swapcase()
