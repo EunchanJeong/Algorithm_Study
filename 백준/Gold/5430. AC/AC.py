55029e8d31948b7b8096b56598aef6ca599c932e
Eunchan Jeong <89077219+EunchanJeong@users.noreply.github.com>
2024-02-21 14:24:29 +0900

    [Gold V] Title: AC, Time: 220 ms, Memory: 45680 KB -BaekjoonHub

---
+++ 백준/Gold/5430. AC/AC.py
@@ -0,0 +1,43 @@
+import sys
+from collections import deque
+
+# N = int(sys.stdin.readline())
+N = int(input())
+error = 0
+
+for _ in range(N):
+    reverse_flag = 0
+    error = 0
+
+    # commands = sys.stdin.readline()
+    # size = int(sys.stdin.readline())
+    commands = input()
+    size = int(input())
+
+    tmp = input().replace('[', '').replace(']', '').split(',')
+    dq = deque(tmp)
+    if '' in tmp:
+        dq.pop()
+
+    for command in commands:
+        if command == 'R':
+            reverse_flag += 1
+        elif command == 'D':
+            if len(dq) == 0:
+                print('error')
+                error = 1
+                break
+            else:
+                if reverse_flag % 2 == 0:
+                    dq.popleft()
+                else:
+                    dq.pop()
+    
+    if error == 1:
+        continue
+    else:
+        if reverse_flag % 2 == 0:
+            print('['+','.join(dq)+']')
+        else:
+            dq.reverse()
+            print('['+','.join(dq)+']')
