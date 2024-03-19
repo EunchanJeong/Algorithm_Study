7e92b7af710a37d554a804d989b6cc9ed2c01b74
Eunchan Jeong <89077219+EunchanJeong@users.noreply.github.com>
2024-03-19 19:27:44 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+N =int(input())
+
+for i in range(N):
+    n = int(input())
+    dp = [0, 1, 2, 4]
+
+    for i in range(4, n+1):
+        dp.append(dp[i-3] + dp[i-2] + dp[i-1])
+
+    print(dp[n])
