1c8478523dd7e7deef182293055bf2e4967b99fc
Eunchan Jeong <89077219+EunchanJeong@users.noreply.github.com>
2024-12-27 14:40:24 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int num) {
+        long n = num;
+        if(n == 1) {
+            return 0;
+        }
+        
+        int count = 0;
+        
+        while(n != 1) {
+            if(n % 2 == 0) {
+                n /= 2;
+            }
+            else {
+                n = (n * 3) + 1;
+            }
+            
+            count++;
+            
+            if(count > 500) {
+                return -1;
+            }
+        }
+        return count;
+    }
+}
