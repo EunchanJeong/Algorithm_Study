2347ac4bd54a8cd9190d99b7892061aa17756f65
Eunchan Jeong <89077219+EunchanJeong@users.noreply.github.com>
2025-01-08 14:40:17 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.69 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -10,5 +10,5 @@ def solution(n):
         dp[2] = 2
 
         for i in range(3, n+1):
-            dp[i] = dp[i-1] + dp[i-2]
-    return dp[n]%1234567
+            dp[i] = (dp[i-1] + dp[i-2]) % 1234567
+    return dp[n]
