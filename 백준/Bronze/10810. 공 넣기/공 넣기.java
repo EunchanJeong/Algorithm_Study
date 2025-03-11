bcf8009ae710d6edb8a4cb3a3b90fc1279f91241
Eunchan Jeong <89077219+EunchanJeong@users.noreply.github.com>
2025-03-11 22:01:17 +0900

    [Bronze III] Title: 공 넣기, Time: 124 ms, Memory: 15924 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer strtk = new StringTokenizer(br.readLine()," ");
+		
+		int basket = Integer.parseInt(strtk.nextToken());
+		int count = Integer.parseInt(strtk.nextToken()); 
+		
+		int n1, n2, ball = 0;
+		
+		int array1[] = new int[basket];
+		
+		for(int i=0;i<count;i++) {
+			strtk = new StringTokenizer(br.readLine()," ");
+			
+			n1 = Integer.parseInt(strtk.nextToken())-1;
+			n2 = Integer.parseInt(strtk.nextToken())-1;
+			ball = Integer.parseInt(strtk.nextToken());
+			
+			for(int j=n1;j<=n2;j++) {
+				array1[j] = ball;
+			}
+		}
+		
+		
+		for(int i=0;i<array1.length;i++) {
+			System.out.print(array1[i]+" ");
+		}
+		
+	}
+}
